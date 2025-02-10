25566b8a8a67abfeda3060926cd9ee0f1e1ca147
thaivinhle <111054941+thaivinhle@users.noreply.github.com>
2025-02-10 03:14:11 -0800

    Add files via upload

---
+++ assignment5/script.js
@@ -0,0 +1,44 @@
+let todo = JSON.parse(localStorage.getItem("todo")) || [];
+
+const todoInput = document.getElementById("todoInput");
+const todoList = document.getElementById("todoList");
+const todoCount = document.getElementById("todoCount");
+const addButton = document.querySelector(".btn");
+const deleteButton = document.getElementById("deleteButton");
+
+document.addEventListener("DOMContentLoaded", function () {
+    addButton.addEventListener("click", addTask);
+    todoInput.addEventListener("keydown", function (event) {
+        if (event.key === "Enter") {
+            event.preventDefault();
+            addTask();
+        }
+    });
+    deleteButton.addEventListener("click", deleteAllTasks);
+    displayTask();
+});
+
+function addTask() {
+    const newTask = todoInput.ariaValueMax.trim();
+    if (newTask !== "") {
+        todo.push({
+            text: newTask,
+            disabled: false,
+        });
+        saveToLocalStorage();
+        todoInput.value = "";
+        displayTask();
+    }
+}
+
+function deleteAllTasks() {
+
+}
+
+function displayTask() {
+
+}
+
+function saveToLocalStorage() {
+    localStorage.setItem("todo", JSON.stringify(todo));
+}
